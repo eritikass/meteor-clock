9c712613cd855d04652f0c646a0f37d039399578
eritikass <eritikass@gmail.com>
2017-04-08 15:23:36 +0300

    build by branch, tag as latest

--- build_locally.sh
+++ build_locally.sh
@@ -2,10 +2,15 @@
 
 ./add_buildinfo.sh
 
+GIT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
 meteor build --directory meteor_build
 
-docker build -t eritikass/meteor-clock .
-docker push eritikass/meteor-clock
+docker build -t eritikass/meteor-clock:$GIT_BRANCH .
+docker push eritikass/meteor-clock:$GIT_BRANCH
+
+docker tag eritikass/meteor-clock:$GIT_BRANCH eritikass/meteor-clock:latest
+docker push eritikass/meteor-clock:latest
 
 # cleanup
 rm -fr meteor_build
