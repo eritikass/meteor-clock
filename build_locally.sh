6c6522eeeed45186e723866cafbc7240046bbe09
eritikass <eritikass@gmail.com>
2017-04-08 13:19:01 +0300

    build docker image with travis-ci

--- build_locally.sh
+++ build_locally.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-meteor build --directory /tmp/export-meteor/build
-cat >/tmp/export-meteor/Dockerfile <<ENDHERE
-FROM ulexus/meteor
-COPY build /home/meteor/www
-RUN chown -R meteor:meteor /home/meteor/
-ENDHERE
-cd /tmp/export-meteor
+meteor build --directory meteor_build
+
 docker build -t eritikass/meteor-clock .
 docker push eritikass/meteor-clock
+
+# cleanup
+rm -fr meteor_build
