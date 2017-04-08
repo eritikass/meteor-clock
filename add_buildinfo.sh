fe7b0b50def1443949c2adabac868279c323cdf9
eritikass <eritikass@gmail.com>
2017-04-08 14:56:58 +0300

    add branch info

--- add_buildinfo.sh
+++ add_buildinfo.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 GIT_COMMIT=$(git log --pretty=format:'%H' -n 1)
+GIT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 BUILD_DATE=$(TZ=":UTC" date)
-echo "{\"git_commit\": \"$GIT_COMMIT\", \"travis_build\": \"$TRAVIS_BUILD_ID\", \"date\": \"$BUILD_DATE\"}" > public/build.json
+echo "{\"git\": {\"commit\": \"$GIT_COMMIT\", \"branch\": \"$GIT_BRANCH\"}, \"travis_build\": \"$TRAVIS_BUILD_ID\",  \"date\": \"$BUILD_DATE\"}" > public/build.json
