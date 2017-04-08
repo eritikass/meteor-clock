5fb1756cea164e2213330bfce69b23be85cc1a8c
eritikass <eritikass@gmail.com>
2017-04-08 13:51:03 +0300

    add build (version) info into docker image

--- build_locally.sh
+++ build_locally.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+BUILD_GITREV=$(git log --pretty=format:'%H' -n 1)
+echo "{\"commit\": \"$BUILD_GITREV\", \"travis_build\": \"$TRAVIS_BUILD_ID\"}" > public/git.json
+
 meteor build --directory meteor_build
 
 docker build -t eritikass/meteor-clock .
